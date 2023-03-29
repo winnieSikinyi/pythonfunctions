7e1b2b9a0b0bb05a91f0f42fa25c1db0e79840f7
winnieSikinyi <winniesikinyi@gmail.com>
2023-03-29 19:45:21 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,5 @@
+def hello(name):
+    print(f"Hello {name}")
+
+def welcome(student,school) :
+    print(f"welcome to {school},{student}")   
---
+++ math.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    answer = a+b
+    return answer
+
+def subtract(c,d):
+    answer = c-d
+    return answer
+
+def divide(e,f):
+    answer = e/f
+    return answer
+
+def multiply(g,h):
+    answer = g*h
+    return answer
+
+def remainder(i,j):
+    answer =i%j
+    return answer
+
+
