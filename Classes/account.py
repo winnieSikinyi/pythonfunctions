c835e9b828e6c169db72fd2c853f65492f3edfaf
winnieSikinyi <winniesikinyi@gmail.com>
2023-05-25 09:44:25 +0300

    Added more methods  on class  account

--- Classes/account.py
+++ Classes/account.py
@@ -27,3 +27,51 @@ class Account:
 # line like this
 # deposit - 1000
 # withdrawal - 500
+
+
+class Account:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+        self.deposits = []
+        self.withdrawals = []
+        self.loan_balance = 0
+    def check_balance(self):
+        return self.balance
+    def deposit(self, amount):
+        self.balance += amount
+        self.deposits.append({
+            "amount": amount,
+            "narration": "deposit"
+        })
+    def withdrawal(self, amount):
+        if self.balance < amount:
+            raise ValueError("Insufficient balance")
+        self.balance -= amount
+        self.withdrawals.append({
+            "amount": amount,
+            "narration": "withdrawal"
+        })
+    def print_statement(self):
+        for transaction in self.deposits + self.withdrawals:
+            print(f"{transaction['narration']} - {transaction['amount']}")
+    def borrow_loan(self, amount):
+        if self.loan_balance > 0:
+            raise ValueError("Account has outstanding loan")
+        if amount < 100:
+            raise ValueError("Loan amount must be more than 100")
+        if len(self.deposits) < 10:
+            raise ValueError("Customer must make at least 10 deposits")
+        if amount > sum(d["amount"] for d in self.deposits) / 3:
+            raise ValueError("Loan amount must be less than or equal to 1/3 of total deposits")
+        self.loan_balance += amount
+    def repay_loan(self, amount):
+        if self.loan_balance < amount:
+            raise ValueError("Loan amount is more than outstanding loan")
+        self.loan_balance -= amount
+        self.balance += amount
+    def transfer(self, amount, other_account):
+        if self.balance < amount:
+            raise ValueError("Insufficient balance")
+        self.balance -= amount
+        other_account.balance += amount
